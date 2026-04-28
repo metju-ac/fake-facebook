ed481a7759b3e0ba6c8b71b901075625d295e0f8
Matej Klima <matej.klima5@gmail.com>
2026-04-28 13:22:03 +0200

    feat: intro

--- src/config/intro.ts
+++ src/config/intro.ts
@@ -2,8 +2,7 @@ import type { Intro } from '../types'
 
 export const intro: Intro = {
   job: { title: 'Software Engineer', company: 'GoodData' },
-  education: ['Gymnázium Plzeň', 'ČVUT FEL Praha'],
-  livesIn: 'Praha, Česká republika',
-  from: 'Plzeň, Česká republika',
-  relationship: 'Ve vztahu',
+  education: ['Gymnázium Mozartova Pardubice', 'MUNI FI Brno'],
+  livesIn: 'Brno, Česká republika',
+  from: 'Pardubice, Česká republika',
 }
